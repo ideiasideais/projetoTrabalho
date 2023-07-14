3918208fb7727deed0d04aab561b11ae5f2fa20b
ideiasideais <esportesgeral.sortudo@gmail.com>
2023-07-13 22:14:30 -0300

    13.07.2023

---
+++ Docs/topo.php
@@ -0,0 +1,11 @@
+<div class="col-sm-12" id="topo">
+    <div id="logo">
+        <div class="container">
+            <div class="row sm-12">
+                <div class="col-sm-4 border border-dark">fff</div>
+                <div class="col-sm-4 border border-dark">ggg</div>
+                <div class="col-sm-4 border border-dark">hhh</div>
+            </div>
+        </div>
+    </div>
+</div>
